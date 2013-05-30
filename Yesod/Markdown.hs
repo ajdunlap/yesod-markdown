ff5f0e70097af84c0320f0fa80501b79e2094831
patrick brisbin <pbrisbin@gmail.com>
2013-05-30 08:26:18 -0400

    Derive PersistFieldSql too

--- Yesod/Markdown.hs
+++ Yesod/Markdown.hs
@@ -38,6 +38,7 @@ import Data.Text.Encoding (decodeUtf8With)
 import Data.Text.Encoding.Error (lenientDecode)
 
 import Database.Persist (PersistField)
+import Database.Persist.Sql (PersistFieldSql)
 import System.Directory (doesFileExist)
 
 import Text.Blaze (ToMarkup (toMarkup))
@@ -55,7 +56,7 @@ import qualified Data.ByteString as B
 import qualified Data.Text       as T
 
 newtype Markdown = Markdown { unMarkdown :: Text }
-    deriving (Eq, Ord, Show, Read, PersistField, IsString, Monoid)
+    deriving (Eq, Ord, Show, Read, PersistField, PersistFieldSql, IsString, Monoid)
 
 instance ToMarkup Markdown where
     -- | Sanitized by default
