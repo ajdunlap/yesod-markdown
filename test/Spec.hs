6dd3a25031c433cf1bef512b3d85d2ce647e5cfe
patrick brisbin <pbrisbin@gmail.com>
2018-09-23 09:54:44 -0400

    Support pandoc-2.2.3
    No actual changes, just something our tests cared about.

--- test/Spec.hs
+++ test/Spec.hs
@@ -29,14 +29,20 @@ spec = describe "Yesod.Markdown" $ do
 
         let Right html = markdownToHtml markdown
 
-        renderHtml html `shouldBe` TL.concat
+        let rendered = renderHtml html
+        rendered `shouldBe` TL.concat
             [ "<h1 id=\"title\">Title</h1>"
             , "<ul>"
             , "<li>one</li>"
             , "<li>two</li>"
             , "<li>three</li>"
             , "</ul>"
-            , "\n  alert('xxs');\n"
+            -- pandoc-2.2.3 changes quote escaping in a minor way that we don't
+            -- care about. CPP is annoying, so let's just make the assertion
+            -- dynamic
+            , if "&#39;" `TL.isInfixOf` rendered
+                then "\n  alert(&#39;xxs&#39;);\n"
+                else "\n  alert('xxs');\n"
             ]
 
     it "converts Markdown to unsanitized HTML" $ do
